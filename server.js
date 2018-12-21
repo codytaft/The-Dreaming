49ca7226de34143382bed9753089b714d543369b
Cody Taft <cody.taft@gmail.com>
2018-12-21 15:59:20 -0700

    Change to express server

--- server.js
+++ server.js
@@ -1,70 +1,77 @@
-var port = 8000;
-var serverUrl = '127.0.0.1';
+let express = require('express');
+let app = express();
+let path = require('path');
 
-var http = require('http');
-var path = require('path');
-var fs = require('fs');
-var checkMimeType = true;
+app.use(express.static('public'));
+// app.use(express.static(__dirname + '/Scripts'));
 
-console.log('Starting web server at ' + serverUrl + ':' + port);
+app.get('/', (req, res) => {
+  res.sendFile('index.html');
+});
 
-http
-  .createServer(function(req, res) {
-    var now = new Date();
+app.listen(3000, () => {
+  console.log('Express Server now running on port 3000');
+});
 
-    var filename = req.url || 'index.html';
-    var ext = path.extname(filename);
-    var localPath = __dirname;
-    var validExtensions = {
-      '.html': 'text/html',
-      '.js': 'application/javascript',
-      '.css': 'text/css',
-      '.txt': 'text/plain',
-      '.jpg': 'image/jpeg',
-      '.gif': 'image/gif',
-      '.png': 'image/png',
-      '.woff': 'application/font-woff',
-      '.woff2': 'application/font-woff2'
-    };
+// console.log('Starting web server at ' + serverUrl + ':' + port);
 
-    var validMimeType = true;
-    var mimeType = validExtensions[ext];
-    if (checkMimeType) {
-      validMimeType = validExtensions[ext] != undefined;
-    }
+// http
+//   .createServer(function(req, res) {
+//     var now = new Date();
 
-    if (validMimeType) {
-      localPath += filename;
-      fs.exists(localPath, function(exists) {
-        if (exists) {
-          console.log('Serving file: ' + localPath);
-          getFile(localPath, res, mimeType);
-        } else {
-          console.log('File not found: ' + localPath);
-          res.writeHead(404);
-          res.end();
-        }
-      });
-    } else {
-      console.log(
-        'Invalid file extension detected: ' + ext + ' (' + filename + ')'
-      );
-    }
-  })
-  .listen(port, serverUrl);
+//     var filename = req.url || 'index.html';
+//     var ext = path.extname(filename);
+//     var localPath = __dirname;
+//     var validExtensions = {
+//       '.html': 'text/html',
+//       '.js': 'application/javascript',
+//       '.css': 'text/css',
+//       '.txt': 'text/plain',
+//       '.jpg': 'image/jpeg',
+//       '.gif': 'image/gif',
+//       '.png': 'image/png',
+//       '.woff': 'application/font-woff',
+//       '.woff2': 'application/font-woff2'
+//     };
 
-function getFile(localPath, res, mimeType) {
-  fs.readFile(localPath, function(err, contents) {
-    if (!err) {
-      res.setHeader('Content-Length', contents.length);
-      if (mimeType != undefined) {
-        res.setHeader('Content-Type', mimeType);
-      }
-      res.statusCode = 200;
-      res.end(contents);
-    } else {
-      res.writeHead(500);
-      res.end();
-    }
-  });
-}
+//     var validMimeType = true;
+//     var mimeType = validExtensions[ext];
+//     if (checkMimeType) {
+//       validMimeType = validExtensions[ext] != undefined;
+//     }
+
+//     if (validMimeType) {
+//       localPath += filename;
+//       fs.exists(localPath, function(exists) {
+//         if (exists) {
+//           console.log('Serving file: ' + localPath);
+//           getFile(localPath, res, mimeType);
+//         } else {
+//           console.log('File not found: ' + localPath);
+//           res.writeHead(404);
+//           res.end();
+//         }
+//       });
+//     } else {
+//       console.log(
+//         'Invalid file extension detected: ' + ext + ' (' + filename + ')'
+//       );
+//     }
+//   })
+//   .listen(port, serverUrl);
+
+// function getFile(localPath, res, mimeType) {
+//   fs.readFile(localPath, function(err, contents) {
+//     if (!err) {
+//       res.setHeader('Content-Length', contents.length);
+//       if (mimeType != undefined) {
+//         res.setHeader('Content-Type', mimeType);
+//       }
+//       res.statusCode = 200;
+//       res.end(contents);
+//     } else {
+//       res.writeHead(500);
+//       res.end();
+//     }
+//   });
+// }
