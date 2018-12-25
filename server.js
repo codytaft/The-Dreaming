5b232db9303e98ac0feab8cf628708ef7d125181
Cody Taft <cody.taft@gmail.com>
2018-12-25 13:45:04 -0600

    Organize components

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ let express = require('express');
 let app = express();
 
 app.use(express.static('public'));
-// app.use(express.static(__dirname + '/src'));
 
 app.get('/', (req, res) => {
   res.sendFile('index.html');
