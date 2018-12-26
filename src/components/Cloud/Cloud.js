d5a4b50c25e48808cc4aea6e01c7c91bf102ccb1
Cody Taft <cody.taft@gmail.com>
2018-12-25 19:52:44 -0600

    Make cloud component

---
+++ src/components/Cloud/Cloud.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Cloud = () => {
+  return (
+    <main className="word-cloud">
+      <h1 className="cloud">Word cloud goes here </h1>
+    </main>
+  );
+};
+
+export default Cloud;
