811c1b954ad25b400b172a9f481c8f61ab126d4b
Cody Taft <cody.taft@gmail.com>
2018-12-25 19:54:55 -0600

    Add home component

---
+++ src/components/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <main className="app-home">
+      <h1 className="main-body">Your dreams go here</h1>
+    </main>
+  );
+};
+
+export default Home;
