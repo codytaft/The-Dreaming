9eff7aa0a8348a53972fea81f8ed7edc1230f378
Cody Taft <cody.taft@gmail.com>
2018-12-25 19:53:28 -0600

    Add header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="app-header">
+      <h1 className="main-title">The Dreaming </h1>
+    </header>
+  );
+};
+
+export default Header;
